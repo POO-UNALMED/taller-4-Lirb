885273e3d50ce2b032c07c7780f23bb3d5b8618a
Antonio José Moreno Saavedra <ajmorenos@unal.edu.co>
2020-09-07 08:48:23 -0500

    Entrega

--- src/main/java/classroom/Persona.java
+++ src/main/java/classroom/Persona.java
@@ -2,15 +2,19 @@ package classroom;
 
 public class Persona {
 
-    final long cedula;
+    long cedula;
     String nombre;
     static int totalPersonas;
     
     static {
         totalPersonas = 0;
-        cedula = 3;
     }
-
+    Persona(){
+    	this.cedula = 0;
+    	this.nombre = "";
+    	totalPersonas++;
+    	
+    }
     Persona(long cedula, String nombre) {
         this.cedula = cedula;
         this.nombre = nombre;
@@ -31,6 +35,7 @@ public class Persona {
 
     Persona(String nombre) {
         this.nombre = "";
+        this.cedula = 1;
         totalPersonas++;
     }
     
